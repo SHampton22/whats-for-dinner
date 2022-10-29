6e6664c46099353aed30cc4a116c8324f1e187c8
Sarah Hampton <stoluba@gmail.com>
2022-10-29 12:51:23 -0400

    Complete iteration1

--- main.js
+++ main.js
@@ -1,55 +1,55 @@
 var sides = [
-    "Miso Glazed Carrots",
-    "Coleslaw",
-    "Garden Salad",
-    "Crispy Potatoes",
-    "Sweet Potato Tots",
-    "Coconut Rice",
-    "Caeser Salad",
-    "Shrimp Summer Rolls",
-    "Garlic Butter Mushrooms",
-    "Hush Puppies"
+    "Miso Glazed Carrots!",
+    "Coleslaw!",
+    "Garden Salad!",
+    "Crispy Potatoes!",
+    "Sweet Potato Tots!",
+    "Coconut Rice!",
+    "Caeser Salad!",
+    "Shrimp Summer Rolls!",
+    "Garlic Butter Mushrooms!",
+    "Hush Puppies!",
     ];
     
-var mains = [
-    "Spaghetti and Meatballs",
-     "Pineapple Chicken",
-     "Shakshuka",
-     "Thai Yellow Curry",
-     "Bibimbap",
-     "Chicken Parmesean",
-     "Butternut Squash Soup",
-     "BBQ Chicken Burgers",
-     "Ramen",
-     "Empanadas",
-     "Chicken Fried Rice",
-     "Sheet Pan Fajitas",
-     "Margarita Pizza"
+var mainDishes = [
+    "Spaghetti and Meatballs!",
+     "Pineapple Chicken!",
+     "Shakshuka!",
+     "Thai Yellow Curry!",
+     "Bibimbap!",
+     "Chicken Parmesean!",
+     "Butternut Squash Soup!",
+     "BBQ Chicken Burgers!",
+     "Ramen!",
+     "Empanadas!",
+     "Chicken Fried Rice!",
+     "Sheet Pan Fajitas!",
+     "Margarita Pizza!",
      ];
         
 var desserts = [
-     "Apple Pie",
-     "Lemon Meringue Pie",
-     "Black Forest Cake",
-     "Banana Bread",
-     "Peach Cobbler",
-     "Cheesecake",
-     "Funfetti Cake",
-     "Baklava",
-     "Flan",
-     "Macarons",
-     "Macaroons",
-     "Chocolate Cupcakes",
-     "Pavlova",
-     "Pumpkin Pie",
-     "Key Lime Pie",
-     "Tart Tatin",
-     "Croissants",
-      "Eclairs"
+     "Apple Pie!",
+     "Lemon Meringue Pie!",
+     "Black Forest Cake!",
+     "Banana Bread!",
+     "Peach Cobbler!",
+     "Cheesecake!",
+     "Funfetti Cake!",
+     "Baklava!",
+     "Flan!",
+     "Macarons!",
+     "Macaroons!",
+     "Chocolate Cupcakes!",
+     "Pavlova!",
+     "Pumpkin Pie!",
+     "Key Lime Pie!",
+     "Tart Tatin!",
+     "Croissants!",
+      "Eclairs!",
      ];
 
      var sidesIndexNum = getRandomIndex(sides);
-     var mainsIndexNum = getRandomIndex(mains);
+     var mainDishesIndexNum = getRandomIndex(mainDishes);
      var dessertsIndexNum = getRandomIndex(desserts);
 
 
@@ -58,20 +58,41 @@ var desserts = [
      var sideSelection = document.querySelector('#side');
      var mainDishSelection = document.querySelector('#main-dish');
      var dessertSelection = document.querySelector('#dessert');
-     var letsCookButton = document.querySelector('.cook-button');
+     var randomRecipe = document.querySelector('#random-recipe');
+     var suggestionBox = document.querySelector('.suggestion-box')
 
+     var removeSuggestionButton = document.querySelector('#remove-suggestion-button')
+     var letsCookButton = document.querySelector('.cook-button');
+     
+
+   
 
      letsCookButton.addEventListener('click', showRandomDish);
+     removeSuggestionButton.addEventListener('click', function(){ hideShow(suggestionBox, potImage) });
+
 
      function getRandomIndex(array) {
         return Math.floor(Math.random() * array.length);
      }
 
      function showRandomDish() {
+        hideShow(potImage, suggestionBox);
+
+        if (sideSelection.checked === true) {
+           randomRecipe.innerHTML = sides[sidesIndexNum];
+        } else if (mainDishSelection.checked === true) {
+             randomRecipe.innerHTML = mainDishes[mainDishesIndexNum];
+        } else if (dessertSelection.checked === true) {
+             randomRecipe.innerHTML = desserts[dessertsIndexNum];
+        }
         
      }
 
-     function hidePot() {
-        potImage.classList.add('hidden');
+     function hideShow(hide, show) {
+        hide.classList.add('hidden');
+        show.classList.remove('hidden');
+     }
 
+     function clearUserInput() {
+        hideShow(suggestionBox, pot)
      }
